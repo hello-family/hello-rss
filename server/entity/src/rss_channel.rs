022ae3c2ccd456c541df32d3a15f1182b92df4c1
ning <15996204@qq.com>
2022-04-28 21:50:43 +0800

    add channel table

---
+++ server/entity/src/rss_channel.rs
@@ -0,0 +1,42 @@
+use sea_orm::entity::prelude::*;
+
+#[derive(Clone, Debug, PartialEq, DeriveEntityModel)]
+#[sea_orm(table_name = "rss_channel")]
+pub struct Model {
+    #[sea_orm(primary_key)]
+    pub id: i32,
+    pub user_id: i32,
+    pub url: String,
+    pub title: String,
+    pub description: String,
+    pub language: Option<String>,
+    pub pub_date: Option<String>,
+    pub last_build_date: Option<String>,
+    pub docs: Option<String>,
+    pub image: Option<String>,
+    pub skip_hours: Json,
+    pub skip_days: Json,
+    pub ttl: Option<i32>,
+    pub update_at: DateTimeUtc,
+    pub create_at: DateTimeUtc,
+}
+
+#[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
+pub enum Relation {
+    #[sea_orm(
+        belongs_to = "super::user::Entity",
+        from = "Column::UserId",
+        to = "super::user::Column::Id",
+        on_update = "NoAction",
+        on_delete = "NoAction"
+    )]
+    User,
+}
+
+impl Related<super::client::Entity> for Entity {
+    fn to() -> RelationDef {
+        Relation::User.def()
+    }
+}
+
+impl ActiveModelBehavior for ActiveModel {}
---
+++ server/migration/src/m20220428_000004_create_rss_channel_table.rs
@@ -0,0 +1,114 @@
+use entity::*;
+use sea_schema::migration::prelude::*;
+
+pub struct Migration;
+
+impl MigrationName for Migration {
+    fn name(&self) -> &str {
+        "m20220428_000004_create_rss_channel_table"
+    }
+}
+// pub docs: Option<String>,
+// pub image: Option<String>,
+// pub skip_hours: DateTimeUtc,
+// pub skip_days: DateTimeUtc,
+// pub ttl: Option<i32>,
+// pub update_at: DateTimeUtc,
+// pub create_at: DateTimeUtc,
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table(rss_channel::Entity)
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new(rss_channel::Column::Id)
+                            .integer()
+                            .auto_increment()
+                            .primary_key()
+                    )
+                    .col(
+                        ColumnDef::new(rss_channel::Column::UserId)
+                            .integer()
+                            .not_null()
+                    )
+                    .col(
+                        ColumnDef::new(rss_channel::Column::Url)
+                            .string()
+                            .not_null()
+                    )
+                    .col(
+                        ColumnDef::new(rss_channel::Column::Title)
+                            .string()
+                            .not_null()
+                    )
+                    .col(
+                        ColumnDef::new(rss_channel::Column::Description)
+                            .string()
+                            .not_null()
+                    )
+                    .col(
+                        ColumnDef::new(rss_channel::Column::Language)
+                            .string()
+                            .string_len(20)
+                    )
+                    .col(
+                        ColumnDef::new(rss_channel::Column::PubDate)
+                            .timestamp()
+                    )
+                    .col(
+                        ColumnDef::new(rss_channel::Column::LastBuildDate)
+                            .timestamp()
+                    )
+                    .col(
+                        ColumnDef::new(rss_channel::Column::Docs)
+                            .string()
+                    )
+                    .col(
+                        ColumnDef::new(rss_channel::Column::Image)
+                            .string()
+                    )
+                    .col(
+                        ColumnDef::new(rss_channel::Column::SkipHours)
+                            .json()
+                    )
+                    .col(
+                        ColumnDef::new(rss_channel::Column::SkipDays)
+                            .json()
+                    )
+                    .col(
+                        ColumnDef::new(rss_channel::Column::Ttl)
+                            .integer()
+                    )
+                    .col(
+                        ColumnDef::new(rss_channel::Column::CreateAt)
+                            .timestamp()
+                            .not_null()
+                    )
+                    .col(
+                        ColumnDef::new(rss_channel::Column::UpdateAt)
+                            .timestamp()
+                            .not_null()
+                    )
+                    .to_owned(),
+            )
+            .await?;
+        manager
+            .create_foreign_key(
+                ForeignKey::create()
+                    .name("fk_user")
+                    .from(rss_channel::Entity, rss_channel::Column::UserId)
+                    .to(user::Entity, user::Column::Id)
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(access_token::Entity).to_owned())
+            .await
+    }
+}
